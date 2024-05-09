b16cf78af91916c5b4831509b84eaee766cfa954
Vasily Gerrans <vgerrans@gmail.com>
2024-05-09 18:08:40 +0100

    feat: import openzeppelin

--- Index.sol
+++ Index.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Index {
+import { AccessControl } from "./node_modules/@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Index is AccessControl{
     uint256 public number;
 }
